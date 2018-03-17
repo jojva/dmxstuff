7f143f1cdafba23d6059e7f8a1e8dc6967a02eff
Joris Valette <joris.valette@gmail.com>
2018-03-17 22:24:12 +0100

    Rollback dmxd.c to its original (untouched by fredex42) version

--- dmxd/dmxd.c
+++ dmxd/dmxd.c
@@ -18,8 +18,6 @@
 #include <sys/shm.h>
 #include <string.h>
 
-//#define DUMP_CHANNELS 0
-
 struct usb_bus *bus;
 struct usb_device *dev;
 usb_dev_handle *udev;
@@ -32,16 +30,7 @@ int shmid;
 
 void write_command ( unsigned char *data )
 {
-int r;
-
-	//putchar('.');
-	//fflush(stdout);
-  r=usb_interrupt_write(udev,0x1,(char*)data,8,20);
-  if(r<0){
-	printf("usb_interrupt_write returned %d\n",r);
-	fflush(stdout);
-//	abort();
-  }
+  usb_interrupt_write(udev,0x1,(char*)data,8,20);
 }
 
 void sendDMX()
@@ -51,20 +40,8 @@ void sendDMX()
   int m;
 
   m=*maxchannel;
-  for (i=0;(i<100) && !channels[i] && (i < m - 6);i++); 
+  for (i=0;(i<100) && !channels[i] && (i < m - 6);i++);
 
-#ifdef DUMP_CHANNELS
-for(i=0;i<255;++i){
-	printf("%02x ",channels[i]);
-	++n;
-	if(n>255/8){
-		printf("\n");
-		n=0;
-	}
-}
-printf("\n\n");
-fflush(stdout);
-#endif
   data[0] = 4; /* Start of data, number of leading 0 and 6 channels */
   data[1] = i+1;
   data[2] = channels[i];
@@ -78,17 +55,18 @@ fflush(stdout);
 
   while (i < m - 7) {
     if (!channels[i]) {
-      for(n=i+1;(n < m - 6) && (n-i<100) && !channels[n] ;n++);
+      for(n=i+1;(n < m - 6) && (n-i<100) && !channels[n] ;n++) {
         data[0] = 5;
         data[1] = n-i;
-	data[2] = channels[n];
-	data[3] = channels[n+1];
-	data[4] = channels[n+2];
-	data[5] = channels[n+3];
-	data[6] = channels[n+4];
-	data[7] = channels[n+5];
-	write_command(data);
-	i=n+6;
+    data[2] = channels[n];
+    data[3] = channels[n+1];
+    data[4] = channels[n+2];
+    data[5] = channels[n+3];
+    data[6] = channels[n+4];
+    data[7] = channels[n+5];
+    write_command(data);
+    i=n+6;
+      }
     } else {
       data[0] = 2; /* 7 channels */
       data[1] = channels[i];
@@ -102,7 +80,7 @@ fflush(stdout);
       i+=7;
     }
   }
-  
+
   for(;i < m;i++) {
     data[0] = 3; /* send one channel */
     data[1] = channels[i];
@@ -112,7 +90,7 @@ fflush(stdout);
 
 int initUSB()
 {
-int r;
+  int count = 0;
 
   usb_init();
   usb_find_busses();
@@ -123,16 +101,11 @@ int r;
       if ( (dev->descriptor.idVendor == 0x10cf) &&
            (dev->descriptor.idProduct == 0x8062 ) ) {
            udev=usb_open(dev);
-	   printf("debug: usb_open gave 0x%x\n",udev);
 #if defined(LIBUSB_HAS_GET_DRIVER_NP) && defined(LIBUSB_HAS_DETACH_KERNEL_DRIVER_NP)
            usb_detach_kernel_driver_np( udev, 0);
 #endif
            usb_set_configuration(udev, 1);
-           r=usb_claim_interface(udev, 0);
-	   if(r<0){
-		printf("Error: usb_claim_interface returned %d\n",r);
-		abort();
-	   }
+           usb_claim_interface(udev, 0);
            return 1;
       }
     }
@@ -142,7 +115,7 @@ int r;
 
 void initSHM()
 {
-    
+
     shmid=shmget(0x56444D58,sizeof(int)*522,IPC_CREAT | 0666);
     shm=(int *)shmat(shmid,NULL,0);
     memset(shm,0,sizeof(int)*522);
@@ -152,7 +125,7 @@ void initSHM()
     channels=shm+10;
     *shutdown=0; /* Run mode */
     *caps=0; /* Services available flags */
-    *maxchannel=22;
+    *maxchannel=6;
 }
 
 void release()
@@ -171,11 +144,11 @@ void timediff(struct timeval *res, struct timeval *a, struct timeval *b) {
         sec--;
     }
     if (sec<0) {
-	res->tv_sec=0;
-	res->tv_usec=0;
+    res->tv_sec=0;
+    res->tv_usec=0;
     } else {
-	res->tv_sec=sec;
-	res->tv_usec=usec;
+    res->tv_sec=sec;
+    res->tv_usec=usec;
     }
 }
 
@@ -183,8 +156,8 @@ void timeadd(struct timeval *res, struct timeval *a, struct timeval *b) {
     res->tv_usec=a->tv_usec+b->tv_usec;
     res->tv_sec=a->tv_sec+b->tv_sec;
     while (res->tv_usec >= 1000000) {
-	res->tv_usec-=1000000;
-	res->tv_sec++;
+    res->tv_usec-=1000000;
+    res->tv_sec;
     }
 }
 
@@ -192,28 +165,28 @@ int main() {
     struct timeval now,next,diff,delay;
 
     if (initUSB()) {
-	initSHM();
+    initSHM();
         delay.tv_sec=0;
-        delay.tv_usec=250000;
+        delay.tv_usec=25000;
         gettimeofday(&next,NULL); /* First tick is now */
         while(!*shutdown) {
-	    if (*maxchannel<22)
-	        *maxchannel=22;
-	    sendDMX();
-	    timeadd(&next,&next,&delay); /* Wait to next tick */
-	    gettimeofday(&now,NULL);
-	    timediff(&diff,&next,&now); /* how much to wait */
-	    while (diff.tv_sec || diff.tv_usec) {
-	        select(0,NULL,NULL,NULL,&diff);
-	        gettimeofday(&now,NULL);
-	        timediff(&diff,&next,&now);
-	    };		    
-	}
-	memset(channels,0,512*sizeof(int));
-	*maxchannel=512;
-	sendDMX();
-	release();
+        if (*maxchannel<6)
+            *maxchannel=6;
+        sendDMX();
+        timeadd(&next,&next,&delay); /* Wait to next tick */
+            gettimeofday(&now,NULL);
+        timediff(&diff,&next,&now); /* how much to wait */
+        while (diff.tv_sec || diff.tv_usec) {
+            select(0,NULL,NULL,NULL,&diff);
+            gettimeofday(&now,NULL);
+            timediff(&diff,&next,&now);
+        };
+        }
+    memset(channels,0,512*sizeof(int));
+    *maxchannel=512;
+    sendDMX();
+    release();
     } else {
-	printf("No DMX device found\n");
+    printf("No DMX device found\n");
     }
 }
