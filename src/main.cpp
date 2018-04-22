e1a3e5ecc4f26b89e9e1f4b298a3a08b34ce1464
Joris Valette <joris.valette@gmail.com>
2018-04-22 16:01:41 +0200

    Handle SIGINT

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "synesthesizer.h"
 
+#include <signal.h>
+
+void sig_handler(int signum)
+{
+    std::cout << "Ctrl+C received. Exiting synesthesizer." << std::endl;
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
+    signal(SIGINT, sig_handler);
     CSynesthesizer Synesthesizer;
     Synesthesizer.Init(argc, argv);
     while(true)
